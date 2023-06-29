59d83e0d7043c02e2bc60cab6a08b12652959216
“mosco98” <moscolyno98@gmail.com>
2023-06-29 15:22:59 +0200

    fixed migrations

---
+++ prisma/migrations/20230629131841_version_3/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Social" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "todoId" TEXT NOT NULL,
+    "isTesting" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Social_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Social" ADD CONSTRAINT "Social_todoId_fkey" FOREIGN KEY ("todoId") REFERENCES "Todo"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
